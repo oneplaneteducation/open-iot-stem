e69e815fda14ce491c760a43c30fa0836d307211
Nickeytheblade <93241171+Nickeytheblade@users.noreply.github.com>
2022-11-02 21:51:43 -0700

    Update decoder2.js

--- decoder2.js
+++ decoder2.js
@@ -6,9 +6,9 @@ function Decoder(bytes, port) {
   // if (port === 1) decoded.led = bytes[0];function Decoder(bytes, port) {
   var TEMP    = (bytes[0] << 4);
   var HUM     = (bytes[4] << 8);  
-  var ALT     = (bytes[10] << 11);
-  var IAQ     = (bytes[11] << 12);
-  var CO2     = (bytes[15] << 17);
+  var ALT     = (bytes[8] << 9);
+  var IAQ     = (bytes[10] << 11);
+  var CO2     = (bytes[11] << 17);
   
   return {
   Temperature:     TEMP,
